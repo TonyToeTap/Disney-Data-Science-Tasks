904bbaf5b313d3b6a0dd4316eff33c88affae3b9
TonyToeTap <lukasjunger@MacBook-Pro-143.fritz.box>
2021-01-27 16:28:58 +0100

    Akuteller Stand des Projektes
    Task3 fast abgeschlossen. Nur noch der Subtask dates->datetime object ist ausstehend.

--- helper/conversion.py
+++ helper/conversion.py
@@ -1,13 +1,42 @@
-'''
-TODO
+import re
 
-Given either a string or a list of strings as input, return
-a number (int or float) which is equal to the monetary value
+number = r"\d+(,\d{3})*\.*\d*"
+amounts = r"thousand|million|billion"
 
-money_conversion("$12.2 million") --> 12200000
-money_conversion("$790,000") --> 790000
+word_re = rf"\${number}(-|\sto\s)?({number})?\s({amounts})"
+value_re = rf"\${number}"
 
-use test_money_conversion.py to test your solution
-'''
+
+def word_to_value(word):
+    value_dict = {"thousand": 1000, "million": 1000000, "billion": 1000000000}
+    return value_dict[word]
+
+
+def parse_word_syntax(string):
+    value = float((re.search(number, string).group()).replace(",", ""))
+    word = re.search(amounts, string, flags=re.I).group().lower()
+    word_value = word_to_value(word)
+    return value*word_value
+
+
+def parse_value_syntax(string):
+    value = float((re.search(number, string).group()).replace(",", ""))
+    return value
+
+
 def money_conversion(money):
-	pass
+
+    if isinstance(money, list):
+        money = money[0]
+
+    word_syntax = re.search(word_re, money, flags=re.I)
+    value_syntax = re.search(value_re, money)
+
+    if word_syntax:
+        return parse_word_syntax(word_syntax.group())
+
+    elif value_syntax:
+        return parse_value_syntax(value_syntax.group())
+
+
+print(money_conversion("$790 millions"))
